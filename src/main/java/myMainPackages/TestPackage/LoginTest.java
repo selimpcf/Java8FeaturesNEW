597f5c5d8a2edd3136b3bc55a041dabe665e5e70
ASG <ahmetgulgonul@gmail.com>
2020-07-22 16:08:05 -0400

    my commit from commandline

---
+++ src/main/java/myMainPackages/TestPackage/LoginTest.java
@@ -0,0 +1,11 @@
+package myMainPackages.TestPackage;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+    @Test
+    public void test1(){
+        System.out.println("test1");
+    }
+}
