79737dd15aee31ec405f20e270f7312268425efb
ASG <ahmetgulgonul@gmail.com>
2020-07-23 12:40:31 -0400

    2

--- src/main/java/myMainPackages/TestPackage/LoginTest.java
+++ src/main/java/myMainPackages/TestPackage/LoginTest.java
@@ -1,11 +1,29 @@
 package myMainPackages.TestPackage;
 
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class LoginTest {
+    
+    @BeforeMethod
+    public void before() {
+        System.out.println("before method");
+    }
+
+    @BeforeTest
+    public void start() {
+        System.out.println("test start");
+    }
 
     @Test
-    public void test1(){
+    public void test1() {
         System.out.println("test1");
     }
+
+    @Test
+    public void test2() {
+        System.out.println("Test 2");
+    }
+
 }
